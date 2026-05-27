fd9998fed71bcd8bcedd9770eff11d745f59925a
Steffen Deusch <steffen@deusch.me>
2026-05-27 11:19:15 +0000

    pinctrl: samsung: add initial Zumapro PERIC0 support

--- drivers/pinctrl/samsung/pinctrl-samsung.h
+++ drivers/pinctrl/samsung/pinctrl-samsung.h
@@ -407,5 +407,6 @@ extern const struct samsung_pinctrl_of_match_data fsd_of_data;
 extern const struct samsung_pinctrl_of_match_data gs101_of_data;
 extern const struct samsung_pinctrl_of_match_data s3c64xx_of_data;
 extern const struct samsung_pinctrl_of_match_data s5pv210_of_data;
+extern const struct samsung_pinctrl_of_match_data zumapro_of_data;
 
 #endif /* __PINCTRL_SAMSUNG_H */

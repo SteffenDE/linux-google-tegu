82b401e9570aa734447a668b036dc27e517cf82c
Steffen Deusch <steffen@deusch.me>
2026-05-31 15:58:52 +0000

    drm/exynos: keep Zumapro DPU scaffold passive

--- drivers/gpu/drm/exynos/exynos_drm_drv.c
+++ drivers/gpu/drm/exynos/exynos_drm_drv.c
@@ -173,7 +173,7 @@ static struct exynos_drm_driver_info exynos_drm_drivers[] = {
 		0
 	}, {
 		DRV_PTR(zumapro_decon_driver, CONFIG_DRM_EXYNOS_ZUMAPRO_DPU),
-		DRM_COMPONENT_DRIVER
+		0
 	}, {
 		DRV_PTR(mixer_driver, CONFIG_DRM_EXYNOS_MIXER),
 		DRM_COMPONENT_DRIVER

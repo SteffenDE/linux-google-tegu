081d265c113799568eeee01293acee00daa88fa0
Steffen Deusch <steffen@deusch.me>
2026-08-20 16:30:04 +0000

    dt-bindings: interconnect: add Google Zumapro BTS

---
+++ include/dt-bindings/interconnect/google,zumapro.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Interconnect IDs for Google Zumapro.
+ */
+
+#ifndef __DT_BINDINGS_INTERCONNECT_GOOGLE_ZUMAPRO_H
+#define __DT_BINDINGS_INTERCONNECT_GOOGLE_ZUMAPRO_H
+
+#define ZUMAPRO_BTS_MASTER_ISPFE		0
+#define ZUMAPRO_BTS_SLAVE_MIF			1
+
+#endif /* __DT_BINDINGS_INTERCONNECT_GOOGLE_ZUMAPRO_H */

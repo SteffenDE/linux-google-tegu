Trijal08/kernel-mainline

drm/panthor: scale every clock the OPP table describes
panthor takes up to three clocks -- core, stacks and coregroup -- but only
ever prepares and enables the last two. Frequency scaling goes through
dev_pm_opp_set_rate(), which drives the single clock the OPP core took from
index 0, so on a SoC where those clocks are separately programmable DVFS
domains the auxiliary ones look wired up in DT and silently never move.

That is the case on zumapro (Tensor G4). "core" and "coregroup" are two
independent ACPM DVFS domains, each with its own PLL and its own PMIC rail,
and the firmware moves only the domain it is asked for -- verified on
silicon by requesting 302 MHz on the shader domain and reading both PLLs:
PLL_G3D went 0xa07a0502 -> 0xa0f50502 (150 -> 301 MHz) while PLL_G3D_L2 sat
at 0xa0820502 (160 MHz) throughout. The vendor driver programs both per
operating point for that reason.

The OPP core already models this: name the clocks in dev_pm_opp_set_config()
and each OPP carries one rate per clock in "opp-hz". Take the names from
SoC data so nothing changes for platforms whose table describes clock #0
only, and apply the OPP rather than the rate when they are set, since
dev_pm_opp_set_rate() is single-clock by construction.

Also switch the fast_rate lookup to dev_pm_opp_find_freq_floor_indexed(),
as the non-indexed form asserts a single clock and would now fail.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
(cherry picked from commit fe472d284c1207500de1e753852c4ee9908424ab)

drivers/gpu/drm/panthor/panthor_drv.c
@@ -1881,9 +1881,18 @@ static const struct panthor_soc_data soc_data_mediatek_mt8196 = {
 	.asn_hash = { 0xb, 0xe, 0x0, },
 };
 
+/*
+ * The GPU sits on two independent DVFS domains, each with its own PLL and its
+ * own rail: "core" clocks the shader stacks and "coregroup" the top-level/L2
+ * block. The firmware that owns them moves only the one it is asked for, so
+ * both rates come from the OPP table.
+ */
+static const char * const zuma_opp_clk_names[] = { "core", "coregroup", NULL };
+
 static const struct panthor_soc_data soc_data_google_zuma = {
 	.asn_hash_enable = true,
 	.asn_hash = { 0x0036db6d, 0x005b6db6, 0x0, },
+	.opp_clk_names = zuma_opp_clk_names,
 };
 
 static const struct of_device_id dt_match[] = {

SteffenDE/linux-google-tegu

drivers/gpu/drm/panthor/panthor_drv.c
@@ -1871,9 +1871,18 @@ static const struct panthor_soc_data soc_data_mediatek_mt8196 = {
 	.asn_hash = { 0xb, 0xe, 0x0, },
 };
 
+/*
+ * The GPU sits on two independent DVFS domains, each with its own PLL and its
+ * own rail: "core" clocks the shader stacks and "coregroup" the top-level/L2
+ * block. The firmware that owns them moves only the one it is asked for, so
+ * both rates come from the OPP table.
+ */
+static const char * const zumapro_opp_clk_names[] = { "core", "coregroup", NULL };
+
 static const struct panthor_soc_data soc_data_google_zumapro = {
 	.asn_hash_enable = true,
 	.asn_hash = { 0x0036db6d, 0x005b6db6, 0x0, },
+	.opp_clk_names = zumapro_opp_clk_names,
 };
 
 static const struct of_device_id dt_match[] = {